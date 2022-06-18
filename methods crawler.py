752895d49f702282545b2192659e9b8ae7d71bd3
m7moudzo3bi <m7moudzo3bi@gmail.com>
2022-06-18 06:48:08 +0300

    add more enhancement

--- methods crawler.py
+++ methods crawler.py
@@ -10,17 +10,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from glob import glob
 import csv
+from datetime import datetime
+
+
+
+now = datetime.now() 
+date_time = now.strftime("%Y%m%d%H%M%S")
 
 driver = webdriver.Chrome('E:/My project/Python/chromedriver.exe')
 
-folder_path = 'E:/My project/Workspace/BatBat-Project JavaDoc/doc/al/tonikolaba/gamestate'
+folder_path = 'E:\My project\Python\Dataset1'
 urls = glob(folder_path + "/*.html")
 
-file_name = "methods"
-with open(file_name+'.csv', 'w', newline='') as file:
+file_name = "methods - " + date_time
+with open("results/"+ file_name +'.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Class", "Methods"])
 
+    occurrence_dict = {}
+    occurrence_loc_dict = {}
     for page_url in urls:
         driver.get(page_url)
         class_title = driver.title    
@@ -29,18 +37,35 @@ with open(file_name+'.csv', 'w', newline='') as file:
            
             for x in z:
                 writer.writerow([class_title, x.text])
+                if(x.text not in occurrence_dict.keys()):
+                    occurrence_dict.update({ x.text : 1 })
+                    occurrence_loc_dict.update({ x.text : class_title })
+                else:
+                   occ = occurrence_dict.get(x.text)
+                   occurrence_dict.update({x.text : occ + 1})
+                   
+                   occ_loc = occurrence_loc_dict.get(x.text)
+                   occurrence_loc_dict.update({ x.text : occ_loc + "," + class_title })
+                    
                 print(class_title, x.text)
             
-        
-        except Exception:
+        except Exception as e :
+            print (e)
             print(class_title + "failed")
             pass
         
         writer.writerow([])
+        
+    writer.writerow(["Method", "# of occurrence"])
+    for x in occurrence_dict.keys():
+        writer.writerow([x, occurrence_dict.get(x)])
+
+    for k in occurrence_loc_dict.keys():
+        writer.writerow([k, occurrence_loc_dict.get(k)])
 
 driver.close()
     
-       
+
 
 
 
